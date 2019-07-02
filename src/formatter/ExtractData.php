6e9d665aff0b654bfe4d31f96e65c909094647bf
pangpond <pondzz@gmail.com>
2019-07-02 19:21:37 +0700

    add tests

--- src/formatter/ExtractData.php
+++ src/formatter/ExtractData.php
@@ -3,8 +3,37 @@ namespace nextgensoft\formatter;
 
 class ExtractData
 {
+    const PERCENT_100 = 100;
+
     public function extractParagraph($paragraph)
     {
-        return trim($paragraph);
+        return explode("\n", trim($paragraph));
+    }
+
+    public function commonTrimSchoolName($schoolName)
+    {
+        $schoolName = str_replace('โรงเรียน', '', $schoolName);
+        $schoolName = str_replace(' ', '', $schoolName);
+        $schoolName = str_replace('1', '', $schoolName);
+        $schoolName = str_replace('.', '', $schoolName);
+        $schoolName = str_replace('ชื่อ', '', $schoolName);
+
+        return $schoolName;
+    }
+    public function compareSchool($array)
+    {
+        return $array['name'];
+    }
+    public function findSchoolName($schoolName)
+    {
+        $schoolName = $this->commonTrimSchoolName($schoolName);
+        foreach (SCHOOL_SUGGEST as $schoolSuggest) {
+            foreach ($schoolSuggest['synonyms'] as $synonyms) {
+                similar_text($schoolName, $synonyms, $perc);
+                if ($perc == self::PERCENT_100) {
+                    return $schoolSuggest['name'];
+                }
+            }
+        }
     }
 }
--- tests/ExtractDataTest.php
+++ tests/ExtractDataTest.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/formatter/ExtractData.php';
 require __DIR__ . '/../src/formatter/FormatData.php';
+require __DIR__ . '/../src/data/paragraph.php';
 require __DIR__ . '/ValidationData.php';
 
 use nextgensoft\formatter\ExtractData;
@@ -17,19 +18,55 @@ class ExtractDataTest extends TestCase
     protected function setup(): void
     {
         $this->formatData = new FormatData(1);
-        // $this->validationData = new ValidationData(1);
+        $this->validationData = new ValidationData(1);
         $this->extractData = new ExtractData();
     }
 
-    public function testStudentCodeShouldBeNumericOnly()
+    public function testExtractParagraph()
     {
-        $studentCode = $this->formatData->formatStudentCode(' 02874 ');
-        $this->assertTrue(is_numeric($studentCode));
+        $paragraphArray = $this->extractData->extractParagraph(DEFAULT_PARAGRAPH);
+        $this->assertCount(4, $paragraphArray);
+
+        $schoolName = $paragraphArray[0];
+        $this->assertGreaterThan(1, strpos($schoolName, 'สระบุรีวิทยาคม'));
+
+        // $data['schoolName'] = array_search($schoolName, SCHOOL_SUGGEST);
+
+        // $data['schoolName'] = array_search($schoolName, array_column(SCHOOL_SUGGEST));
+
+        // var_dump($data['schoolName']);exit;
+
+        // $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        // $this->assertEquals('สระบุรีวิทยาคม', $data['schoolName']);
 
         // $studentCode = $this->formatData->formatStudentCode('002874');
         // $this->assertTrue(is_numeric($studentCode));
+        // $this->assertEquals(trim(DEFAULT_PARAGRAPH), $paragraph);
 
         // $studentCode = $this->formatData->formatStudentCode('07465eee');
         // $this->assertTrue(is_numeric($studentCode));
     }
+    public function testExtractSchoolName()
+    {
+        $schoolName = '1.ชื่อโรงเรียน สระบุรีวิทยาคม';
+        $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        $this->assertEquals('สระบุรีวิทยาคม', $data['schoolName']);
+
+        $schoolName = '1.ชื่อโรงเรียน สบว';
+        $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        $this->assertEquals('สระบุรีวิทยาคม', $data['schoolName']);
+
+        $schoolName = '1.ชื่อโรงเรียน สระบุรี';
+        $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        $this->assertEquals('สระบุรีวิทยาคม', $data['schoolName']);
+
+        $schoolName = 'สระบุรี';
+        $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        $this->assertEquals('สระบุรีวิทยาคม', $data['schoolName']);
+
+        $schoolName = '1.ชื่อโรงเรียน การุ้ง';
+        $data['schoolName'] = $this->extractData->findSchoolName($schoolName);
+        $this->assertEquals('การุ้งวิทยาคม', $data['schoolName']);
+    }
+
 }
